e6673f40bab0641e9b21ce97ff29dbf1081107b5
nstarke <starke.nicholas@gmail.com>
2024-05-26 11:36:22 -0500

    optimizing for time

--- BruteForceDiscover.py
+++ BruteForceDiscover.py
@@ -2,6 +2,23 @@
 
 import argparse, subprocess, random, operator, json
 
+def bruteforce(prefix, suffix, filename, languageId):
+    map = dict()
+    for i in range(256):
+        n = '%030x' % random.randrange(16**30)
+        cmd = '/opt/ghidra-11.0.3/support/analyzeHeadless /tmp ' + n + ' -import ' + filename + ' -postScript CountReferencedStrings.java -processor ' + languageId + ' -loader BinaryLoader -loader-baseAddr ' + prefix + ('%02d' % i) + suffix + ' -deleteProject | grep CountReferencedStrings.java'
+        output = subprocess.check_output(cmd, shell=True, text=True)
+        referenced = output[output.find("<referenced>") + len("<referenced>"):output.find("</referenced>")]
+        total = output[output.find("<total>") + len("<total>"):output.find("</total>")]
+        referenced = int(referenced)
+        total = int(total)
+        e = {'base': i, 'total': total, 'referenced': referenced}
+        map[i] = e
+
+    s = sorted(map, key=operator.itemgetter('referenced'), reverse=True)
+    print(s[0]['base'])
+    return s[0]
+
 def main():
     parser = argparse.ArgumentParser(
         prog="BruteForceDiscover", 
@@ -10,23 +27,15 @@ def main():
     parser.add_argument('languageId')
 
     args = parser.parse_args()
-    map = dict()
-    for i in range(65535):
-        n = '%030x' % random.randrange(16**30)
-        cmd = '/opt/ghidra-11.0.3/support/analyzeHeadless /tmp ' + n + ' -import ' + args.filename + ' -postScript CountReferencedStrings.java -processor ' + args.languageId + ' -loader BinaryLoader -loader-baseAddr ' + hex(i) + '0000 -deleteProject | grep CountReferencedStrings.java'
-        output = subprocess.check_output(cmd, shell=True, text=True)
-        referenced = output[output.find("<referenced>") + len("<referenced>"):output.find("</referenced>")]
-        total = output[output.find("<total>") + len("<total>"):output.find("</total>")]
-        referenced = int(referenced)
-        total = int(total)
-        e = {'base': i, 'total': total, 'referenced': referenced}
-        map[i] = e
+    octet1 = bruteforce('', '000000', args.filename, args.languageId)
+    octet2 = bruteforce(('%02d' % octet1['base']), '0000', args.filename, args.languagedId)
+    octet3 = bruteforce(('%02d%02d' % octet1['base'], octet2['base']), '00', args.filename, args.languagedId)
+    octet4 = bruteforce(('%02d%02d%02d' % octet1['base'], octet2['base'], octet3['base']), '', args.filename, args.languagedId)
     
-    s = sorted(map, key=operator.itemgetter('referenced'), reverse=True)
-    winner = s[0]
-    print(winner)
-    with open('results.json', 'w') as r:
-        r.write(json.dumps(s))
+    base = ( '%02d%02d%02d%02d' % octet1['base'], octet2['base'], octet3['base'], octet4['base'])
+    print('Winner: ' + base)
+    with open('results.txt', 'w') as r:
+        r.write(base)
 
 if __name__ == "__main__":        
     main()
