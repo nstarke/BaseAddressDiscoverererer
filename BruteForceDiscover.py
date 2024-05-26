5f886a2cbff151c65c989aee836a65cc57d6bb96
nstarke <starke.nicholas@gmail.com>
2024-05-26 09:51:21 -0500

    adding BruteForcing

---
+++ BruteForceDiscover.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import argparse, subprocess, random, operator, json
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="BruteForceDiscover", 
+        description="A script that takes raw binary programs and bruteforces their load offset")
+    parser.add_argument('filename')
+
+    args = parser.parse_args()
+    map = dict()
+    for i in range(65535):
+        n = '%030x' % random.randrange(16**30)
+        cmd = '/opt/ghidra-11.0.3/support/analyzeHeadless /tmp ' + n + ' -import ' + args.filename + ' -postScript CountReferencedStrings.java -processor "ARM:LE:32:Cortex" -loader BinaryLoader -loader-baseAddr ' + hex(i) + '0000 | grep CountReferencedStrings.java'
+        output = subprocess.check_output(cmd, shell=True, text=True)
+        referenced = output[output.find("<referenced>") + len("<referenced>"):output.find("</referenced>")]
+        total = output[output.find("<total>") + len("<total>"):output.find("</total>")]
+        referenced = int(referenced)
+        total = int(total)
+        e = {'base': i, 'total': total, 'referenced': referenced}
+        map[i] = e
+    
+    s = sorted(map, key=operator.itemgetter('referenced'), reverse=True)
+    winner = s[0]
+    print(winner)
+    with open('results.json', 'w') as r:
+        r.write(json.dumps(s))
+
+if __name__ == "__main__":        
+    main()
---
+++ CountReferencedStrings.java
@@ -0,0 +1,47 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//This script counts the references to existing strings.
+//@category Analysis
+
+import java.util.*;
+
+import ghidra.app.script.GhidraScript;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.mem.Memory;
+import ghidra.program.util.DefinedDataIterator;
+import ghidra.program.model.listing.Data;
+
+public class CountReferencedStrings extends GhidraScript {
+
+	@Override
+	public void run() throws Exception {
+
+		monitor.setMessage("Finding Strings with References");
+		int referencedCount = 0;
+		int totalCount = 0;
+		for (Data nextData: DefinedDataIterator.definedStrings(currentProgram) ) {
+			Address strAddr = nextData.getMinAddress();
+			int refCount = currentProgram.getReferenceManager().getReferenceCountTo(strAddr);
+			totalCount++;
+			if (  refCount > 0 ) {
+				referencedCount++;
+			}
+		}
+
+		println("Number of referenced strings found: <referenced>" + referencedCount + "</referenced>");
+		println("Total number of strings found: <total>" + totalCount + "</total>");
+	}
+}
