28fa2aa84958cac97bfd110a34bf25d0b09cc3c4
nstarke <starke.nicholas@gmail.com>
2024-05-28 15:19:24 -0500

    fixing type error

--- BruteForceDiscover.py
+++ BruteForceDiscover.py
@@ -10,6 +10,7 @@ def run_ghidra(filename, languageId, address, map):
     total = output[output.find("<total>") + len("<total>"):output.find("</total>")]
     referenced = int(referenced)
     total = int(total)
+    address = int(address)
     e = {'base': address, 'total': total, 'referenced': referenced}
     map.append(e)
     with open("results/results-%04x.txt" % (address), 'w') as r:
