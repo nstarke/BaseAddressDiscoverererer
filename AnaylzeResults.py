68c271dcaa9cbc160111540a90c2dee0208d3566
nstarke <starke.nicholas@gmail.com>
2024-06-01 02:06:08 +0000

    adding configurable count to analyze results script

--- AnaylzeResults.py
+++ AnaylzeResults.py
@@ -3,6 +3,14 @@
 import argparse, glob, operator, json, pathlib
 
 def main():
+    parser = argparse.ArgumentParser(
+        prog="AnalyzeResults",
+        description="Analyze results contained in 'results' directory"
+    )
+
+    parser.add_argument('-c', '--count', type=int, default=10, const=10, nargs='?')
+    args = parser.parse_args()
+    
     results = []
     for file in glob.glob("results/**/results-*.json"):
         p = pathlib.Path(file)
@@ -12,7 +20,7 @@ def main():
             results.append(j)
 
     s = sorted(results, key=operator.itemgetter('referenced'), reverse=True)
-    print(json.dumps(s[:10]))
+    print(json.dumps(s[:args.count]))
             
 
 if __name__ == "__main__":
