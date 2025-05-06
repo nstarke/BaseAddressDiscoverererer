0085236d2920eba7ad29c3fffa4a0261d97d5be4
nstarke <starke.nicholas@gmail.com>
2025-05-05 22:17:16 -0500

    adding ghidra_scripts to install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ sudo apt install -y unzip openjdk-21-jdk
 wget -P ~ https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.2_build/ghidra_11.3.2_PUBLIC_20250415.zip
 unzip ~/ghidra_11.3.2_PUBLIC_20250415.zip
 rm ~/ghidra_11.3.2_PUBLIC_20250415.zip
+mkdir ~/ghidra_scripts
+cp ghidra_scripts/* ~/ghidra_scripts/
