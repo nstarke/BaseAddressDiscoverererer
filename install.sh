647e6035fb5322f0b522f60e84e6422fb81d88e0
nick-alephvoid <nick@alephvoid.com>
2025-05-08 19:14:57 -0500

    adding git submodule update to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git submodule update --init --recursive
 sudo apt install -y unzip openjdk-21-jdk
 wget -P ~ https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.2_build/ghidra_11.3.2_PUBLIC_20250415.zip
 unzip ~/ghidra_11.3.2_PUBLIC_20250415.zip -d ~/
