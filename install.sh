bb2fa679baa74481aa4b115aa7763356706d5b1b
nick-alephvoid <nick@alephvoid.com>
2025-05-08 20:08:51 -0500

    adding/activating virtualenv

--- install.sh
+++ install.sh
@@ -36,5 +36,18 @@ fi
 echo "Copying scripts to ~/ghidra_scripts..."
 cp ghidra_scripts/* ~/ghidra_scripts/
 
+if [ ! -d .venv ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+else
+    echo "Virtual environment already exists"
+fi
+
+echo "Activating virtual environment..."
+source .venv/bin/activate
+
+echo "Installing requirements..."
+pip install -r requirements.txt
+
 echo "all done!"
 exit 0
